da6fbaa3d7dfb479c4f0299c48f9dc39cae1324c
anarayan05 <namanjames4@gmail.com>
2025-07-01 21:04:39 -0400

    initial score calculation

--- stop_modeling.py
+++ stop_modeling.py
@@ -1,48 +1,62 @@
 import osmnx as ox
 import pandas as pd
 
-place = "Buffalo, New York, USA"
+place = "Emeryville, California, USA"
 
 G = ox.graph.graph_from_place(place, network_type="drive") #downloads street network
-
-#add or delete features later
-# rail_station = ox.features.features_from_place(place, {"building": "train_station"}) #polygon
-# commercial = ox.features.features_from_place(place, {"landuse": "commercial"}) #polygon
-
-# rail_station_points = rail_station.representative_point() #point
-# commercial_points = commercial.representative_point() #point
-# existing_stops = ox.features.features_from_place(place, {"highway": "bus_stop"}) #point
-# apartments = ox.features.features_from_place(place, {"building": "apartments"}) #point
-# hospital = ox.features.features_from_place(place, {"amenity": "hospital"}) #point
-
-tags = {
-    'building': ['train_station', 'apartments'],
-    'landuse': 'commercial',
-    'highway': 'bus_stop',
-    'building': 'apartments',
-    'amenity': 'hospital'
-}
-
 gdf_nodes = ox.convert.graph_to_gdfs(G, nodes = True, edges = False)
 gdf_nodes_coord = gdf_nodes.get_coordinates()
 
-point_score_dict = {}
+#fig, ax = ox.plot.plot_graph(G) to see nodes
+
+existing_stops = ox.features.features_from_place(place, {"highway": "bus_stop"}) #type; point. dont use this for expirimenting
+
+
+tags = {
+    'building': ['apartments', 'supermarket'],
+    'landuse': ['commercial'],
+    'railway': ['station'],
+    'amenity': ['hospital','school', 'university']
+}
+
+#how each tage is weighted
+#determine values
+tag_score = {
+    'building: apartments': 1,
+    'building: supermarket': 1,
+    'landuse: commercial': 1,
+    'railway: station': 1,
+    'amenity: hospital': 1,
+    'amenity: school': 1,
+    'amenity: university': 1
+}
+
+#matching feature keys and values to tags
+#to get score
+
+#consider adding centrality aspect to calculation (at point)
+
+def calculateScore(features):
+    score = 0
+    for index, row in features.iterrows():
+        key = row.index[1] #grabs the type of feature
+        value = row.iloc[1] #value of of the type
+        if(key in tags.keys()):
+            if(value in tags[key]):
+                score+=tag_score[key+': '+value]
+    return score
+       
+point_score_dict = {}       
 
 for idx, row in gdf_nodes_coord.iterrows():
     lat = row['y']
     long = row['x']
     point = (lat, long)
     try:
-        nearest_features = ox.features.features_from_point(point, tags, dist=300)
-        score = 0 #for now
+        nearest_features = ox.features.features_from_point(point, tags, dist=300) #nearest features from tags to each node
+        score = calculateScore(nearest_features)
         point_score_dict[point] = score
+        print(f"{point}: {score}")
     except ox._errors.InsufficientResponseError as e:
         print("Did not find any features nearby")
-
-
-# lat0 = gdf_nodes_coord['y'].iloc[0]
-# long0 = gdf_nodes_coord['x'].iloc[0]
-# point0 = (lat, long)
-
-# print(point_score_dict[point])
 
